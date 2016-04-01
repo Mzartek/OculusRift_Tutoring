10acbd0a6f676dd0357e00c5ec5fb1460b0d3e0c
Kevin Havranek <mzartek@hotmail.fr>
2016-04-01 21:30:26 +0200

    Just a simple rename

--- Assets/Scripts/ToonEffect.cs
+++ Assets/Scripts/ToonEffect.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine;
 
-public class XToonEffect : MonoBehaviour
+public class ToonEffect : MonoBehaviour
 {
     public Shader shader;
 
+    public int grayScale;
+
     private Material material;
+
+    private Texture2D texture;
 
     private void Start()
     {
         material = new Material(shader);
+        texture = new Texture2D(grayScale, 1);
+
+        for (int i = 0; i < grayScale; i++)
+        {
+            //texture.SetPixel(i, 1, );
+        }
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
